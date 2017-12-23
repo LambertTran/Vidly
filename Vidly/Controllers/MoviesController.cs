68392041f49648df73286b2908ee56fdbeedd33e
Lambert Tran <lb.tran648@gmail.com>
2017-12-23 13:51:58 -0800

    add NEW and EDIT movie route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -32,6 +32,46 @@ namespace Vidly.Controllers
             var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
             return View(movie);
         }
-        
+
+        public ActionResult New()
+        {
+            var movie = new Movie();
+            return View("New", movie);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.NumberInStock= movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Genre = movie.Genre;
+
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+
+            }
+
+            return View("New", movie);
+
+        }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,19 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre Type")]
         public string Genre{ get; set; }
+
+        [Display(Name = "Release Date")]
         public string ReleaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
 
 
